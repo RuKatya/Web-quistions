3a3c7cdc182d04821bdc59a2853ca9d01a2d8b6b
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-19 00:18:20 +0300

    1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express(); //express
 const path = require('path')
+const cors = require('cors');
+app.use(cors())
 
 const PORT = process.env.PORT || 6565; //connect to port 6565
 
