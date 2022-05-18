13084370cb535858e545aa9bfca838b4d144c8b7
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 19:02:00 +0300

    make it better

--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import path from 'path';
 const app = express(); //express
-const PORT = process.env.PORT || 8655;
 
-// app.use(express.static(path.join(__dirname, 'public', 'build')))
-app.use('/', express.static(path.join(__dirname, 'public', 'build')));
-// app.use(express.static(path.join(__dirname, '..', 'public', 'build')));
+const PORT = process.env.PORT || 6565; //connect to port 6565
 
+app.get('/', (req, res) => {
+    try {
+        res.send('we get it')
+    } catch (error) {
+        res.send(`error from server: ${error}`)
+    }
+})
 try {
     app.listen(PORT, () => {
         console.log(`Server listen on http://localhost:${PORT}`)
