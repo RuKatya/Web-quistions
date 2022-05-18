98fcb446541c5f92b9deaeb8bdc778d449be47ec
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 18:28:39 +0300

    make it better

---
+++ dist/server.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default(); //express
+const PORT = process.env.PORT || 6565; //connect to port 6565
+app.get('/', (req, res) => {
+    try {
+        res.send('we get it');
+    }
+    catch (error) {
+        res.send(`error from server: ${error}`);
+    }
+});
+try {
+    app.listen(PORT, () => {
+        console.log(`Server listen on http://localhost:${PORT}`);
+    });
+}
+catch (err) {
+    console.log(err);
+}
