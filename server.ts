cbec8bd22b7f321ba4b2b1084c5d9ecccc0332ef
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 18:47:02 +0300

    make it better

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const app = express(); //express
 const PORT = process.env.PORT || 8655;
 
 // app.use(express.static(path.join(__dirname, 'public', 'build')))
-app.use(express.static(path.join(__dirname, 'public', 'build')));
+app.use(express.static(path.join(__dirname, '', 'public', 'build')));
 
 try {
     app.listen(PORT, () => {
