afaf2cc0f4aa24f27fc733ba5aff23467d2afd5c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 19:16:39 +0300

    make it better

--- dist/server.js
+++ dist/server.js
@@ -5,15 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default(); //express
+const path_1 = __importDefault(require("path"));
 const PORT = process.env.PORT || 6565; //connect to port 6565
-app.get('/', (req, res) => {
-    try {
-        res.send('we get it');
-    }
-    catch (error) {
-        res.send(`error from server: ${error}`);
-    }
-});
+app.use('/', express_1.default.static(path_1.default.join(__dirname, '..', 'public', 'build')));
+// app.get('/', (req, res) => {
+//     try {
+//         res.send('we get it')
+//     } catch (error) {
+//         res.send(`error from server: ${error}`)
+//     }
+// })
 try {
     app.listen(PORT, () => {
         console.log(`Server listen on http://localhost:${PORT}`);
--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 const app = express(); //express
+import path from 'path'
 
 const PORT = process.env.PORT || 6565; //connect to port 6565
 
-app.get('/', (req, res) => {
-    try {
-        res.send('we get it')
-    } catch (error) {
-        res.send(`error from server: ${error}`)
-    }
-})
+app.use('/', express.static(path.join(__dirname, '..', 'public', 'build')));
+// app.get('/', (req, res) => {
+//     try {
+//         res.send('we get it')
+//     } catch (error) {
+//         res.send(`error from server: ${error}`)
+//     }
+// })
 try {
     app.listen(PORT, () => {
         console.log(`Server listen on http://localhost:${PORT}`)
