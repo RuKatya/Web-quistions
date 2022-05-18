35d4fa3820cf07c2599e81bae0d1c39ffc7fd789
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 18:34:24 +0300

    make it better

--- dist/server.js
+++ dist/server.js
@@ -6,14 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default(); //express
 const PORT = process.env.PORT || 6565; //connect to port 6565
-app.get('/', (req, res) => {
-    try {
-        res.send('we get it');
-    }
-    catch (error) {
-        res.send(`error from server: ${error}`);
-    }
-});
+app.use(express_1.default.static('public/build'));
+// app.get('/', (req, res) => {
+//     try {
+//         res.send('we get it')
+//     } catch (error) {
+//         res.send(`error from server: ${error}`)
+//     }
+// })
 try {
     app.listen(PORT, () => {
         console.log(`Server listen on http://localhost:${PORT}`);
--- server.ts
+++ server.ts
@@ -3,13 +3,15 @@ const app = express(); //express
 
 const PORT = process.env.PORT || 6565; //connect to port 6565
 
-app.get('/', (req, res) => {
-    try {
-        res.send('we get it')
-    } catch (error) {
-        res.send(`error from server: ${error}`)
-    }
-})
+app.use(express.static('public/build'))
+// app.get('/', (req, res) => {
+//     try {
+//         res.send('we get it')
+//     } catch (error) {
+//         res.send(`error from server: ${error}`)
+//     }
+// })
+
 try {
     app.listen(PORT, () => {
         console.log(`Server listen on http://localhost:${PORT}`)
