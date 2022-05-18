20556e35f288386daaef99355b1ba02b9a17a89c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-18 19:24:30 +0300

    make it better

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express(); //express
+const path = require('path')
+
+const PORT = process.env.PORT || 6565; //connect to port 6565
+
+express.static(path.join(__dirname, 'public', 'build'));
+// app.get('/', (req, res) => {
+//     try {
+//         res.send('we get it')
+//     } catch (error) {
+//         res.send(`error from server: ${error}`)
+//     }
+// })
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server listen on http://localhost:${PORT}`)
+    })
+} catch (err) {
+    console.log(err)
+}
